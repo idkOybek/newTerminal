83f81eaf5fd7a0a4ea05a7ca527586c4ebd5d067
idkOybek <oybekberkinov3@gmail.com>
2024-07-27 02:37:28 +0500

    asd# Please enter the commit message for your changes. Lines starting

--- docs/docs.go
+++ docs/docs.go
@@ -1109,7 +1109,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "newterminal.onrender.com",
+	Host:             "txkm-vipos.uz",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "New Terminal API",
