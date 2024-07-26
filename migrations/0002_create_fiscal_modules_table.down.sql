1eedede03056185ff74e72c6b1935113bfe044d3
idkOybek <oybekberkinov3@gmail.com>
2024-07-27 00:19:26 +0500

    ьшпкфешщты

---
+++ migrations/0002_create_fiscal_modules_table.down.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS fiscal_modules (
+    id SERIAL PRIMARY KEY,
+    factory_number VARCHAR(255) UNIQUE NOT NULL,
+    fiscal_module_number VARCHAR(255) UNIQUE NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
+);
---
+++ migrations/0003_create_terminals_table.down.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS terminals (
+    id SERIAL PRIMARY KEY,
+    inn VARCHAR(12) NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    address TEXT NOT NULL,
+    cash_register_number VARCHAR(255) UNIQUE NOT NULL,
+    module_number VARCHAR(255) UNIQUE NOT NULL,
+    assembly_number VARCHAR(255) UNIQUE NOT NULL,
+    last_request_date TIMESTAMP WITHOUT TIME ZONE,
+    database_update_date TIMESTAMP WITHOUT TIME ZONE,
+    status BOOLEAN DEFAULT true,
+    user_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
+);
