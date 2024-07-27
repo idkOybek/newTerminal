db374dc76cf588e1299ca1cf33a806561edfbf6e
idkOybek <oybekberkinov3@gmail.com>
2024-07-27 17:15:13 +0500

    lol

--- migrations/0002_create_fiscal_modules_table.up.sql
+++ migrations/0002_create_fiscal_modules_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS fiscal_modules (
     id SERIAL PRIMARY KEY,
     factory_number VARCHAR(255) UNIQUE NOT NULL,
-    fiscal_module_number VARCHAR(255) UNIQUE NOT NULL,
+    fiscal_number VARCHAR(255) UNIQUE NOT NULL,
     user_id INTEGER REFERENCES users(id),
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
